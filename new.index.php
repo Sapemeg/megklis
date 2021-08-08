cf762ab6091bb6ace4e1ee8699c3210548bbcae3
Sarantos Meglis <sarantos@meglis.com>
2021-08-08 21:23:17 +0300

    added html

---
+++ new.index.php
@@ -0,0 +1,205 @@
+<?php 
+$remote_lang = ($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+
+if(!(strpos($remote_lang, "el")!==false)){ ?>
+<!DOCTYPE html>
+        <html>
+        <title> Sarantos Meglis | Sarantos Megklis </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+
+	<meta name="google-site-verification" content="PSq_vEftqJDW-kFszP-icXw1vsNuNPuZJq4TP3asDRg" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png?v=477zgJz20j">
+        <link rel="icon" type="image/png" href="/favicon-32x32.png?v=477zgJz20j" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicon-194x194.png?v=477zgJz20j" sizes="194x194">
+        <link rel="icon" type="image/png" href="/favicon-96x96.png?v=477zgJz20j" sizes="96x96">
+        <link rel="icon" type="image/png" href="/android-chrome-192x192.png?v=477zgJz20j" sizes="192x192">
+	
+
+
+
+	<script>
+function sm_dsb(cityName, k) {
+    var i; 
+    var j;
+    var x = document.getElementsByClassName("nav");
+    var y = document.getElementById("nvg");
+    y.style.display = "block";
+    var z = document.getElementsByClassName("bt");
+    for(j=0; j< z.length ; j++){
+    	z[j].className=z[j].className.replace("w3-disabled", "");
+	//z[j].className=z[j].className.replace("w3-white", "w3-blue");
+   }
+   k.className += "w3-disabled";	
+   //k.className=k.className.replace(" w3-blue", "w3-white");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    document.getElementById(cityName).style.display = "block";  
+}
+</script>
+	
+        <body>
+        <div class="w3-display-container" >
+        	<div class="w3-row" >
+
+
+
+          <div class="w3-card w3-third  w3-center w3-blue  " style:"width=50%;"  >
+            <header class="w3-container w3-blue w3-padding-16  ">
+              	<div class="w3-container  ">
+ 			  <img src="img.gif"  style="width:30%;" >
+		</div>
+		<h1>Sarantos P. Meglis</h1>
+		<p>Undergraduate Student, Athens Dept. of Informatics & Telecommunications</p>
+        	    
+	</header>
+		<div class="w3-bar " >
+		<button class="w3-button w3-indigo w3-padding-large  bt  " onclick="sm_dsb('CV', this)" style="display:none"  ><b>CV</b></button>
+  		<button class="w3-button w3-indigo w3-padding-large  bt " onclick="sm_dsb('Contact', this)"><b>Contact</b></button>
+  		<button class="w3-button w3-indigo w3-padding-large  bt " onclick="sm_dsb('About', this)"><b>About</b></button>
+		</div>    
+	
+            <div id="nvg" class="w3-container  w3-center w3-light-blue " style="display:none"   >
+	
+	<div id="CV" class="w3-container nav" style="display:none">
+ 	</div>
+
+	<div id="Contact" class="w3-container w3-light-blue nav" style="display:none">
+  	 <div class="w3-content w3-center w3-padding">
+	 <a href="https://twitter.com/sapemeg" target="_blank" class=" w3-btn w3-theme-d3"> Twitter </a>
+	<a href="https://facebook.com/sarantos.meglis" target="_blank" class="w3-btn w3-theme-d3" > Facebook </a>
+		<a href="mailto:sarantos@meglis.com">sarantos@meglis.com</a>
+	<a href="SarantosPMegklis.txt" target="_blank" class="w3-btn w3-theme-d5 w3-center" > PGP key </a>	
+</div>
+	</div>
+
+	<div id="About" class="w3-container w3-light-blue  nav" style="display:none">
+  	 <div class="w3-content w3-center w3-padding">
+  	  	<a href="https://blog.meglis.com" target="_blank" class=" w3-btn w3-theme-d3"> blog.meglis.com </a>
+		<a href="http://www.w3schools.com/w3css/" target="_blank" class=" w3-btn w3-theme-d5" >made with w3.css</a>
+	</div>	
+	</div>
+	    </div>
+            <footer class="w3-container w3-lime w3-tiny  ">
+        	    	
+		copyright 2018 Sarantos Meglis
+            </footer>
+          </div>
+        </div>
+        </div>
+        </body>
+        </html>
+
+<?php 
+}else{ ?>
+<!DOCTYPE html>
+        <html>
+        <title>Σαράντος Μεγλής| ΣΑΡΑΝΤΟΣ ΜΕΓΚΛΗΣ </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	
+	<meta name="google-site-verification" content="PSq_vEftqJDW-kFszP-icXw1vsNuNPuZJq4TP3asDRg" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png?v=477zgJz20j">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png?v=477zgJz20j">
+        <link rel="icon" type="image/png" href="/favicon-32x32.png?v=477zgJz20j" sizes="32x32">
+        <link rel="icon" type="image/png" href="/favicon-194x194.png?v=477zgJz20j" sizes="194x194">
+        <link rel="icon" type="image/png" href="/favicon-96x96.png?v=477zgJz20j" sizes="96x96">
+        <link rel="icon" type="image/png" href="/android-chrome-192x192.png?v=477zgJz20j" sizes="192x192">
+
+	
+	<script>
+function sm_dsb(cityName, k) {
+    var i; 
+    var j;
+    var x = document.getElementsByClassName("nav");
+    var y = document.getElementById("nvg");
+    y.style.display = "block";
+    var z = document.getElementsByClassName("bt");
+    for(j=0; j< z.length ; j++){
+    	z[j].className=z[j].className.replace("w3-disabled", "");
+	//z[j].className=z[j].className.replace("w3-white", "w3-blue");
+   }
+   k.className += "w3-disabled";	
+   //k.className=k.className.replace(" w3-blue", "w3-white");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    document.getElementById(cityName).style.display = "block";  
+}
+</script>
+	
+        <body>
+        <div class="w3-display-container" >
+        	<div class="w3-row w3-content w3-padding-16" >
+
+
+
+          <div class="w3-card w3-third  w3-center w3-blue  "  >
+            <header class="w3-container w3-blue w3-padding-16  ">
+              	<div class="w3-container  ">
+ 			  <img src="img.gif"  style="width:30%;" >
+		</div>
+		<h1>Σαράντος Π. Μεγκλής</h1>
+		<p>Προπτυχιακός Φοιτητής, Τμήμα Πληροφορικής & Τηλεπικοινωνιών ΕΚΠΑ</p>
+        	    
+	</header>
+		<div class="w3-bar " >
+		
+  		<button class="w3-button w3-indigo w3-padding-large  bt  " onclick="sm_dsb('CV', this)" style="display:none"  ><b>CV</b></button>
+  		<button class="w3-button w3-indigo w3-padding-large  bt " onclick="sm_dsb('Contact', this)"><b>Επικοινωνία</b></button>
+  		<button class="w3-button w3-indigo w3-padding-large  bt " onclick="sm_dsb('About', this)"><b>Σχετικά</b></button>
+		
+		</div>    
+	
+            <div id="nvg" class="w3-container  w3-center w3-light-blue " style="display:none"   >
+	
+	<div id="CV" class="w3-container nav" style="display:none">
+ 		 <p>text CV </p>
+	</div>
+
+	<div id="Contact" class="w3-container w3-light-blue nav" style="display:none">
+  	 <div class="w3-content w3-center w3-padding">
+	 <a href="https://twitter.com/sapemeg" target="_blank" class=" w3-btn w3-theme-d3"> Twitter </a>
+	<a href="https://facebook.com/sarantos.meglis" target="_blank" class="w3-btn w3-theme-d3" > Facebook </a>
+		<a href="mailto:sarantos@meglis.com">sarantos@meglis.com</a>
+	<a href="SarantosPMegklis.txt" target="_blank" class="w3-btn w3-theme-d5 w3-center" > PGP key </a>	
+</div>
+	</div>
+
+	<div id="About" class="w3-container w3-light-blue  nav" style="display:none">
+  	 <div class="w3-content w3-center w3-padding">
+  	  	<a href="https://blog.meglis.com" target="_blank" class=" w3-btn w3-theme-d3"> blog.meglis.com </a>
+		<a href="http://www.w3schools.com/w3css/" target="_blank" class=" w3-btn w3-theme-d5" >made with w3.css</a>
+	</div>	
+	</div>
+	    </div>
+            <footer class="w3-container w3-lime w3-tiny  ">
+        	    	
+		copyright 2018 Sarantos Meglis
+            </footer>
+          </div>
+        </div>
+        </div>
+        </body>
+        </html>
+
+<?php } ?>
+
+
